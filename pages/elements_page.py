588546853b00e7cb1a4d1232f6e2665325256a25
azubov <zubov.alexey@gmail.com>
2023-03-29 19:20:15 +0300

    1. datagenerator 2. remake test

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,3 +1,4 @@
+from generator.generator import generated_person
 from locators import elements_page_locators
 from pages.base_page import BasePage
 
@@ -5,12 +6,20 @@ from pages.base_page import BasePage
 class TextBoxPage(BasePage):
     locators = elements_page_locators.TextBoxPageLocators
 
-    def fill_all_fields(self, full_name, email, current_address, permanent_address):
+    def fill_all_fields(self):
+        person_info = next(generated_person())
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+
         self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
         self.element_is_visible(self.locators.EMAIL).send_keys(email)
         self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
         self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT).click()
+
+        return full_name, email, current_address, permanent_address
 
     def check_filled_form(self):
         full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
--- tests/test_elements.py
+++ tests/test_elements.py
@@ -8,18 +8,14 @@ class TestElements:
             text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
             text_box_page.open()
 
-            full_name = "John Doe"
-            email = 'asa@mail.com'
-            current_address = '123 Main St'
-            permanent_address = '456 Main St'
             # Act
-            text_box_page.fill_all_fields(full_name, email, current_address, permanent_address)
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             # Assert
             output_full_name, output_email, output_current_address, output_permanent_address = text_box_page.check_filled_form()
-            assert output_full_name == full_name
-            assert output_email == email
-            assert output_current_address == current_address
-            assert output_permanent_address == permanent_address
+            assert output_full_name == full_name, 'Full name is not correct'
+            assert output_email == email, 'Email is not correct'
+            assert output_current_address == current_address, 'Current address is not correct'
+            assert output_permanent_address == permanent_address, 'Permanent address is not correct'
 
 
 
